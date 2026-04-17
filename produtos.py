4cf16daee90648d4db784e6ab3663e35d870d42a
IsaIcebb <167563308+IsaIcebb@users.noreply.github.com>
2026-04-17 14:33:04 -0300

    Update produtos.py

--- produtos.py
+++ produtos.py
@@ -1,24 +1,24 @@
 from flask import Flask, jsonify
 import os
 
-app = Flask(-__name__)
+app = Flask(__name__)
 
 produtos = [
-    {"id": 1, "produtos": "Haikyuu","autor": "Haruichi Furudado"},
-    {"id": 2, "produtos": "Heartstopper","autor": "Alice Oseman"},
-    {"id": 3, "produtos": "Tudo é Rio ","autor": "Carla Madeira"},
-    {"id": 4, "produtos": "A Biblioteca da Meia-Noite","autor": "Mattb Haig"},
+    {"id": 1, "produtos": "Creme de Cabelo","preço": "9 reais"},
+    {"id": 2, "produtos": "Caneta preta","preço": "3 reais"},
+    {"id": 3, "produtos": "Matizador Roxo","preço": "12 reais"},
+    {"id": 4, "produtos": "Anel","preço": "14 reais"},
     
 ]
 
-@app.route("/livros", methods=["GET"])
+@app.route("/produtos", methods=["GET"])
 def home():
-    return jsonify({"mensagem": "API de livros - Acesse /livros"})
+    return jsonify({"mensagem": "API de produtos - Acesse /produtos"})
 
 @app.route("/", methods=["GET"])
-def listar_livros():
-    return jsonify(livros)
+def listar_produtos():
+    return jsonify(produtos)
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT" , 5000))
-    app.run(host="0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
