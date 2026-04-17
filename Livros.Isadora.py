a51d275e2b5f75d074af8d0d24a9a04b84432024
IsaIcebb <167563308+IsaIcebb@users.noreply.github.com>
2026-04-17 14:33:32 -0300

    Update Livros.Isadora.py

--- Livros.Isadora.py
+++ Livros.Isadora.py
@@ -19,6 +19,6 @@ def home():
 def listar_livros():
     return jsonify(livros)
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT" , 5000))
     app.run(host="0.0.0.0", port=port)
